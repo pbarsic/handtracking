77c6419f629efbb56699fc2a1b1bf37de8c43ad2
Paul Barsic <pbarsic@gmail.com>
2019-10-22 16:45:11 -0700

    score is displayed in the boxes

--- utils/detector_utils.py
+++ utils/detector_utils.py
@@ -58,6 +58,7 @@ def draw_box_on_image(num_hands_detect, score_thresh, scores, boxes, im_width, i
             p1 = (int(left), int(top))
             p2 = (int(right), int(bottom))
             cv2.rectangle(image_np, p1, p2, (77, 255, 9), 3, 1)
+            cv2.putText(image_np, str(scores[i]), (int(boxes[i][1] * im_width),  int(boxes[i][2] * im_height- 10)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 3)
 
 
 # Show fps value on image.
